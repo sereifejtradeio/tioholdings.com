505834f716db32a1b1e602d2e2df72f90093cb42
Christos Zenonos <christos.zenonos@tiomarkets.com>
2019-09-23 11:03:49 +0300

    alagoues kai fixes

--- index.php
+++ index.php
@@ -172,6 +172,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!--SCROLL BAR CHANGE COLOR  -->
     <script>
         $(document).ready(function() {
             // executes when HTML-Document is loaded and DOM is ready
@@ -204,6 +205,7 @@
             // document ready
         });
     </script>
+    <!--    ENABLE UTM SCRIPT-->
     <script>
         $(document).ready(function () {
             var anchors = $('a');
@@ -220,7 +222,7 @@
                     }
                 }
             });
-        });'
+        });
     </script>
 </body>
 </html>
