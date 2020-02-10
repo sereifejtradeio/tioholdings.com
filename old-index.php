271cf95dd6464670c37e516a78ec7f3568c7bdb1
Christos Zenonos <christos.zenonos@tiomarkets.com>
2020-02-10 11:16:33 +0200

    website under construction

---
+++ old-index.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="google-site-verification" content="u8paYM47iVfpZ3T8r4ihHpf2ZAYy793-8gEUVYqxjEI" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css" />
+
+    <title>TIO Group</title>
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <meta name="description" content="Top provider of digital marketing services in Cyprus"/>
+    <meta name="og:title" content="TIO Group"/>
+    <meta name="og:type" content="marketing"/>
+    <meta name="og:url" content="https://tioholdings.com/"/>
+    <meta name="og:image" content="https://tioholdings.com/images/tio_groups_th.jpg">
+    <meta name="og:site_name" content="TIOgroup"/>
+    <meta name="og:description" content="Top provider of digital marketing services in Cyprus"/>
+<!--    <meta property="fb:app_id" content="106919353416246"/>-->
+    <meta name="twitter:title" content="TIO Group">
+    <meta name="twitter:description" content="Top provider of digital marketing services in Cyprus">
+    <meta name="twitter:image" content="https://tioholdings.com/images/tio_groups_th.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+<!--    <meta name="twitter:site" content="@TradeToken">-->
+    <meta prefix="og:http://ogp.me/ns#" property="og:title" content="TIO Group"/>
+    <meta prefix="og:http://ogp.me/ns#" property="og:type" content="Marketing"/>
+    <meta prefix="og:http://ogp.me/ns#" property="og:image" content="https://tioholdings.com/images/tio_groups_th.jpg"/>
+    <meta prefix="og:http://ogp.me/ns#" property="og:url" content="https://tioholdings.com/"/>
+</head>
+<body>
+    <nav class="navbar navbar-dark fixed-top align-items-center justify-content-center ">
+        <a class="navbar-brand" href="#">
+<!--            <img src="images/tio-groups-logo-h.png" width="140" alt="tio groups logo">-->
+            <img src="images/tiogroup.png" width="140" alt="tio groups logo">
+        </a>
+    </nav>
+    <!-- 1st section intro -->
+<!--    <section id="intro" class="view hm-black-strong">-->
+<!--        <div-->
+<!--                class="container-fluid full-bg-img d-flex align-items-center justify-content-center h-100"-->
+<!--        >-->
+<!--            <div class="row d-flex justify-content-center">-->
+<!--                <div-->
+<!--                        class="col-md-12 text-center align-items-center justify-content-center"-->
+<!--                >-->
+<!--                    <h1 class="font-bold text-white pt-5">-->
+<!--                        Marketing Company-->
+<!---->
+<!--                    </h1>-->
+<!--                    <h4 class="lead text-white mb-5">-->
+<!--                        TOP PROVIDER OF DIGITAL MARKETING SERVICES IN CYPRUS-->
+<!--                    </h4>-->
+<!--                    <p class="text-white"><img src="images/LocationPin.png" alt="location"> Limassol</p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+
+    <!-- 2nd section gallery -->
+    <section id="services">
+        <div class="container">
+            <div class="row mb-5 pt-5">
+                <div
+                        class="col-md-12 text-center text-white align-items-center justify-content-center mt-5"
+                >
+                    <h2>Top provider of digital marketing services in Cyprus</h2>
+                    <h5>
+<!--                        Top provider of digital marketing services in Cyprus-->
+<!--                        Based out of our headquarters in Limassol, Cyprus, we are a marketing and front-end development company specialising in digital marketing.-->
+                    </h5>
+                </div>
+            </div>
+
+            <div class="row text-white mb-3">
+                <div class="col-lg-3 mb-3 text-center">
+                    <div class="card-transparent">
+                        <img class="card-img-top" src="images/content-icon.png" alt="" />
+                        <div class="card-body">
+                            <h5 class="card-title">Content and Public Relations</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-3 text-center">
+                    <div class="card-transparent">
+                        <img class="card-img-top" src="images/events-services-icon.png" alt="" />
+                        <div class="card-body">
+                            <h5 class="card-title">Events Services</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-3 text-center">
+                    <div class="card-transparent">
+                        <img class="card-img-top" src="images/graphic-design-icon.png" alt="" />
+                        <div class="card-body">
+                            <h5 class="card-title">Graphic Design</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-3 text-center">
+                    <div class="card-transparent">
+                        <img class="card-img-top" src="images/digital-campaigns-icon.png" alt="" />
+                        <div class="card-body">
+                            <h5 class="card-title">Digital Organic and Paid Campaigns </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section 3 Mission and vision-->
+    <section id="mission">
+        <div class="container">
+            <div class="row  mb-5 pb-5">
+                <div
+                        class="col-md-12 text-center align-items-center justify-content-center mt-5 text-white"
+                >
+                    <h2>Our Mission and Vision</h2>
+                    <p>We aim to provide top-level marketing services to help drive both online & offline traffic, foster global recognition, and, ultimately, help to strengthen each of our clients' brand & business.</p>
+<!--                    <p>-->
+<!--                        Our mission is to provide top tier marketing services, to drive both online and offline traffic, to foster global recognition, and thereby strengthen the branding and business operations of those who utilize our services.-->
+<!--                    </p>-->
+<!--                    <p>-->
+<!--                        Our vision is to position ourselves as the top provider of digital marketing services in Cyprus.-->
+<!--                        Contact us to discuss the co-operation.-->
+<!--                    </p>-->
+<!--                    <a href="#" class="btn btn-secondary">Contact us</a>-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="clients">
+        <div class="container">
+            <div class="row pt-5 pb-5">
+                <div class="col-md-12 text-center align-items-center justify-content-center  text-white">
+                    <h2>Our Clients</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 clients-images">
+                    <a href="https://tiomarkets.com" target="_blank"><img src="images/tiomarkets-image-logo.jpg" class="img-fluid" title="Visit Tio Markets" alt="Tio Markets"></a>
+                </div>
+                <div class="col-md-6 clients-images">
+                    <a href="https://trade.io" target="_blank"><img src="images/tradeio-image-logo.jpg" class="img-fluid" title="Visit Trade Io" alt="Trade Io"></a>
+                </div>
+            </div>
+<!--            <div class="col-md-12 text-center align-items-center justify-content-center mt-5 text-white">-->
+<!--                <a href="#" id="clients-button"  class="btn btn-secondary">Become a Client</a>-->
+<!--            </div>-->
+
+        </div>
+    </section>
+
+    <section id="contact-us">
+        <div class="container">
+            <div class="row pt-5 pb-5">
+                <div class="col-md-12 text-center align-items-center justify-content-center mt-5 mb-5 text-white">
+                    <h2>Contact Us</h2>
+                </div>
+                <div class="col-lg-4 text-center align-items-center justify-content-center mt-1 text-white">
+                    <p><i class="fa fa-envelope"></i> <strong>Email: </strong><a href="mailto:info@tioholdings.com"> info@tioholdings.com</a></p>
+                </div>
+                <div class="col-lg-4 text-center align-items-center justify-content-center mt-1 text-white">
+                    <p><i class="fa fa-phone-volume"></i> <strong>Call: </strong>00357 25251008</p>
+                </div>
+                <div class="col-lg-4 text-center align-items-center justify-content-center mt-1 text-white">
+
+                    <p><i class="fa fa-map-marker-alt"></i> <strong>Address: </strong> 80 Athinon Limassol 3040 Cyprus</p>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <section id="footer">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-md-6 text-muted">
+                    <a href="#"><img src="images/tiogroup.png"  alt="Tio Group"></a>
+                    <p class="group-ltd ">T.I.O. Group Holdings Ltd</p>
+                </div>
+                <div class="col-md-6 text-muted">
+                    <p>All rights reserved, 2019</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!--SCROLL BAR CHANGE COLOR  -->
+    <script>
+        $(document).ready(function() {
+            // executes when HTML-Document is loaded and DOM is ready
+
+
+            /*
+            ################
+            Add navbar background color when scrolled
+            */
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 56) {
+                    $(".navbar").addClass("bg-dark");
+                } else {
+                    $(".navbar").removeClass("bg-dark");
+                }
+            });
+            // If Mobile, add background color when toggler is clicked
+            $(".navbar-toggler").click(function() {
+                if (!$(".navbar-collapse").hasClass("show")) {
+                    $(".navbar").addClass("bg-dark");
+                } else {
+                    if ($(window).scrollTop() < 56) {
+                        $(".navbar").removeClass("bg-dark");
+                    } else {
+                    }
+                }
+            });
+            // ############
+
+            // document ready
+        });
+    </script>
+    <!--    ENABLE UTM SCRIPT-->
+    <script>
+        $(document).ready(function () {
+            var anchors = $('a');
+            var utmS = location.search;
+            Object.values(anchors).forEach(function (el, key) {
+                var item = String($(el).attr('href'));
+                if (!item.includes('#') && item !== 'undefined' && item !== "javascript:void(0)" && utmS.includes('utm') && item.includes('http')) {
+                    if (item.includes('?')) {
+                        var existingQueryString = item.match(/(\?)(.)+/g)[0].replace('?', '&');
+                        item = item.replace(item.match(/(\?)(.)+/g)[0], '');
+                        $(el).attr('href', item + utmS + existingQueryString);
+                    } else {
+                        $(el).attr('href', item + utmS);
+                    }
+                }
+            });
+        });
+    </script>
+</body>
+</html>
