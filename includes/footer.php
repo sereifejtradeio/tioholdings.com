7de2e653be53c4e42ee6d29bd808494bf72b95a5
Petros Leonidou <petros.leonidou@trade.io>
2020-02-13 08:44:27 +0200

    rw changed

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>©TIOholdings 2020. All Rights Reserved.</p>
+                <p>© 2020 T.I.O. Group Holdings Ltd. All Rights Reserved.</p>
             </div>
         </div>
     </div>
